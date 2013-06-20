fb34b00be9bedee115f48924aa0154ddb708268e
Paul O’Shannessy <paul@oshannessy.com>
2013-06-20 15:12:37 -0700

    v0.3.3

--- react.js
+++ react.js
@@ -1,5 +1,5 @@
 /**
- * React v0.3.2
+ * React v0.3.3
  */
 (function(e){if("function"==typeof bootstrap)bootstrap("react",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeReact=e}else"undefined"!=typeof window?window.React=e():global.React=e()})(function(){var define,ses,bootstrap,module,exports;
 return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
@@ -1551,11 +1551,15 @@ var ReactMount = {
   renderComponent: function(nextComponent, container) {
     var prevComponent = instanceByReactRootID[getReactRootID(container)];
     if (prevComponent) {
-      var nextProps = nextComponent.props;
-      ReactMount.scrollMonitor(container, function() {
-        prevComponent.replaceProps(nextProps);
-      });
-      return prevComponent;
+      if (prevComponent.constructor === nextComponent.constructor) {
+        var nextProps = nextComponent.props;
+        ReactMount.scrollMonitor(container, function() {
+          prevComponent.replaceProps(nextProps);
+        });
+        return prevComponent;
+      } else {
+        ReactMount.unmountAndReleaseReactRootNode(container);
+      }
     }
 
     ReactMount.prepareTopLevelEvents(ReactEventTopLevelCallback);
@@ -8197,7 +8201,9 @@ var Properties = {
   height: null,
   href: null,
   htmlFor: null,
+  max: null,
   method: null,
+  min: null,
   multiple: MustUseProperty | HasBooleanValue,
   name: null,
   poster: null,
@@ -8211,6 +8217,7 @@ var Properties = {
   selected: MustUseProperty | HasBooleanValue,
   spellCheck: null,
   src: null,
+  step: null,
   style: null,
   tabIndex: null,
   target: null,
